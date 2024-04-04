b9fa6ff6b5aa3da9a628dd255dab45009bec42b3
TeamTMZ <165527852+TeamTMZ@users.noreply.github.com>
2024-04-04 22:31:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -86,8 +86,10 @@ async def start_command(client: Client, message: Message):
         return
     else:
         reply_markup = InlineKeyboardMarkup(
-            [
-                [
+            [[
+            InlineKeyboardButton('🔍 Movie Gʀᴏᴜᴘ', url='https://t.me/MovieszRequest'),
+            InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/TamilMoviesz_HD')
+            ],[
                     InlineKeyboardButton("😊 About Me", callback_data = "about"),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
