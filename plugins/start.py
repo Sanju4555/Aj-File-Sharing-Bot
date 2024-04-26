fd26df502441ceba1d0608184b9a3ffa2276f8ee
Sanju4555 <119828512+Sanju4555@users.noreply.github.com>
2024-04-27 01:30:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -87,8 +87,8 @@ async def start_command(client: Client, message: Message):
     else:
         reply_markup = InlineKeyboardMarkup(
             [[
-            InlineKeyboardButton('🔍 Movie Gʀᴏᴜᴘ', url='https://telegram.me/+Zk-rhQnpXc4wYjE1'),
-            InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://telegram.me/Aj_creations_ajay1')
+            InlineKeyboardButton('🔍 Movie Gʀᴏᴜᴘ', url='https://t.me/Movies_Request_Group_One'),
+            InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/Telugu_OTT_Movies_Updates')
             ],[
                     InlineKeyboardButton("😊 About Me", callback_data = "about"),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
